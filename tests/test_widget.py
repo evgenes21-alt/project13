cbf9f9b801976eec6d859e0a9c53a8f08906ea89
Суспицын Евгений <evgen.es21@gmail.com>
2025-09-08 20:10:42 +0300

    сделал тест

--- tests/test_widget.py
+++ tests/test_widget.py
@@ -4,7 +4,7 @@ from src.widget import get_date, mask_account_card
 
 
 @pytest.mark.parametrize(
-    "card_type_number, result",
+    "card_type_number, masked_number, masked_account"
     [
         ("Maestro 1596837868705199", "Maestro 1596 83** **** 5199"),
         ("Счет 64686473678894779589", "Счет **9589"),
@@ -13,16 +13,15 @@ from src.widget import get_date, mask_account_card
         ("Visa Classic 6831982476737658", "Visa Classic 6831 98** **** 7658"),
         ("Visa Platinum 8990922113665229", "Visa Platinum 8990 92** **** 5229"),
         ("Visa Gold 5999414228426353", "Visa Gold 5999 41** **** 6353"),
-        ("Счет 73654108430135874305", "Счет **4305"),
-    ],
-)
-def test_mask_account_card(card_type_number: str, result: str) -> None:
+        ("Счет 73654108430135874305", "Счет **4305"),],)
+
+def test_mask_account_card(card_type_number: str, masked_number: str,masked_account: str) -> None:
     """Функция передает строку с номером карты или счета"""
-    assert mask_account_card(card_type_number) == result
+    assert mask_account_card(card_type_number) == masked_number
 
 
 @pytest.mark.parametrize(
-    "date, result",
+    "date, dates",
     [
         ("2018-07-11T02:26:18.671407", "11.07.2018"),
         ("2019-07-03T18:35:29.512364", "03.07.2019"),
---
+++ tests/tests_mask.py
@@ -0,0 +1,8 @@
+import pytest
+
+
+from src.masks import get_mask_card_number,get_mask_account
+
+@pytest.mark.parametrize(
+    "card, card_number",
+)
