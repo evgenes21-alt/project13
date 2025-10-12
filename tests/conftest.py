365e9e45a8f41d8ee46fce4a67c9720d7210b3dd
Суспицын Евгений <evgen.es21@gmail.com>
2025-10-12 10:59:36 +0300

    dz 12.1

--- tests/conftest.py
+++ tests/conftest.py
@@ -115,6 +115,8 @@ def mixed_state_data() -> List[Dict[str, Any]]:
         {"id": 103, "state": "FAILED", "date": "2024-01-04T06:30:00"},
         {"id": 104, "state": "EXECUTED", "date": "2024-01-05T18:45:00"},
     ]
+
+
 @pytest.fixture
 def list_of_transactions():
     return [
@@ -124,14 +126,11 @@ def list_of_transactions():
             "date": "2018-06-30T02:08:58.425572",
             "operationAmount": {
                 "amount": "9824.07",
-                "currency": {
-                    "name": "USD",
-                    "code": "USD"
-                }
+                "currency": {"name": "USD", "code": "USD"},
             },
             "description": "Перевод организации",
             "from": "Счет 75106830613657916952",
-            "to": "Счет 11776614605963066702"
+            "to": "Счет 11776614605963066702",
         },
         {
             "id": 142264268,
@@ -139,14 +138,11 @@ def list_of_transactions():
             "date": "2019-04-04T23:20:05.206878",
             "operationAmount": {
                 "amount": "79114.93",
-                "currency": {
-                    "name": "USD",
-                    "code": "USD"
-                }
+                "currency": {"name": "USD", "code": "USD"},
             },
             "description": "Перевод со счета на счет",
             "from": "Счет 19708645243227258542",
-            "to": "Счет 75651667383060284188"
+            "to": "Счет 75651667383060284188",
         },
         {
             "id": 873106923,
@@ -154,14 +150,11 @@ def list_of_transactions():
             "date": "2019-03-23T01:09:46.296404",
             "operationAmount": {
                 "amount": "43318.34",
-                "currency": {
-                    "name": "руб.",
-                    "code": "RUB"
-                }
+                "currency": {"name": "руб.", "code": "RUB"},
             },
             "description": "Перевод со счета на счет",
             "from": "Счет 44812258784861134719",
-            "to": "Счет 74489636417521191160"
+            "to": "Счет 74489636417521191160",
         },
         {
             "id": 895315941,
@@ -169,14 +162,11 @@ def list_of_transactions():
             "date": "2018-08-19T04:27:37.904916",
             "operationAmount": {
                 "amount": "56883.54",
-                "currency": {
-                    "name": "USD",
-                    "code": "USD"
-                }
+                "currency": {"name": "USD", "code": "USD"},
             },
             "description": "Перевод с карты на карту",
             "from": "Visa Classic 6831982476737658",
-            "to": "Visa Platinum 8990922113665229"
+            "to": "Visa Platinum 8990922113665229",
         },
         {
             "id": 594226727,
@@ -184,15 +174,12 @@ def list_of_transactions():
             "date": "2018-09-12T21:27:25.241689",
             "operationAmount": {
                 "amount": "67314.70",
-                "currency": {
-                    "name": "руб.",
-                    "code": "RUB"
-                }
+                "currency": {"name": "руб.", "code": "RUB"},
             },
             "description": "Перевод организации",
             "from": "Visa Platinum 1246377376343588",
-            "to": "Счет 14211924144426031657"
-        }
+            "to": "Счет 14211924144426031657",
+        },
     ]
 
 
@@ -205,14 +192,11 @@ def list_of_transactions_for_regex():
             "date": "2019-08-26T10:50:58.294041",
             "operationAmount": {
                 "amount": "31957.58",
-                "currency": {
-                    "name": "руб.",
-                    "code": "RUB"
-                }
+                "currency": {"name": "руб.", "code": "RUB"},
             },
             "description": "Перевод организации",
             "from": "Maestro 1596837868705199",
-            "to": "Счет 64686473678894779589"
+            "to": "Счет 64686473678894779589",
         },
         {
             "id": 41428829,
@@ -220,14 +204,11 @@ def list_of_transactions_for_regex():
             "date": "2019-07-03T18:35:29.512364",
             "operationAmount": {
                 "amount": "8221.37",
-                "currency": {
-                    "name": "USD",
-                    "code": "USD"
-                }
+                "currency": {"name": "USD", "code": "USD"},
             },
             "description": "Перевод организации",
             "from": "MasterCard 7158300734726758",
-            "to": "Счет 35383033474447895560"
+            "to": "Счет 35383033474447895560",
         },
         {
             "id": 939719570,
@@ -235,14 +216,11 @@ def list_of_transactions_for_regex():
             "date": "2018-06-30T02:08:58.425572",
             "operationAmount": {
                 "amount": "9824.07",
-                "currency": {
-                    "name": "USD",
-                    "code": "USD"
-                }
+                "currency": {"name": "USD", "code": "USD"},
             },
             "description": "Перевод организации",
             "from": "Счет 75106830613657916952",
-            "to": "Счет 11776614605963066702"
+            "to": "Счет 11776614605963066702",
         },
         {
             "id": 587085106,
@@ -250,13 +228,10 @@ def list_of_transactions_for_regex():
             "date": "2018-03-23T10:45:06.972075",
             "operationAmount": {
                 "amount": "48223.05",
-                "currency": {
-                    "name": "руб.",
-                    "code": "RUB"
-                }
+                "currency": {"name": "руб.", "code": "RUB"},
             },
             "description": "Открытие вклада",
-            "to": "Счет 41421565395219882431"
+            "to": "Счет 41421565395219882431",
         },
         {
             "id": 142264268,
@@ -264,14 +239,11 @@ def list_of_transactions_for_regex():
             "date": "2019-04-04T23:20:05.206878",
             "operationAmount": {
                 "amount": "79114.93",
-                "currency": {
-                    "name": "USD",
-                    "code": "USD"
-                }
+                "currency": {"name": "USD", "code": "USD"},
             },
             "description": "Перевод со счета на счет",
             "from": "Счет 19708645243227258542",
-            "to": "Счет 75651667383060284188"
+            "to": "Счет 75651667383060284188",
         },
         {
             "id": 873106923,
@@ -279,14 +251,11 @@ def list_of_transactions_for_regex():
             "date": "2019-03-23T01:09:46.296404",
             "operationAmount": {
                 "amount": "43318.34",
-                "currency": {
-                    "name": "руб.",
-                    "code": "RUB"
-                }
+                "currency": {"name": "руб.", "code": "RUB"},
             },
             "description": "Перевод со счета на счет",
             "from": "Счет 44812258784861134719",
-            "to": "Счет 74489636417521191160"
+            "to": "Счет 74489636417521191160",
         },
         {
             "id": 214024827,
@@ -294,14 +263,11 @@ def list_of_transactions_for_regex():
             "date": "2018-12-20T16:43:26.929246",
             "operationAmount": {
                 "amount": "70946.18",
-                "currency": {
-                    "name": "USD",
-                    "code": "USD"
-                }
+                "currency": {"name": "USD", "code": "USD"},
             },
             "description": "Перевод организации",
             "from": "Счет 10848359769870775355",
-            "to": "Счет 21969751544412966366"
+            "to": "Счет 21969751544412966366",
         },
         {
             "id": 522357576,
@@ -309,14 +275,11 @@ def list_of_transactions_for_regex():
             "date": "2019-07-12T20:41:47.882230",
             "operationAmount": {
                 "amount": "51463.70",
-                "currency": {
-                    "name": "USD",
-                    "code": "USD"
-                }
+                "currency": {"name": "USD", "code": "USD"},
             },
             "description": "Перевод организации",
             "from": "Счет 48894435694657014368",
-            "to": "Счет 38976430693692818358"
+            "to": "Счет 38976430693692818358",
         },
         {
             "id": 895315941,
@@ -324,14 +287,11 @@ def list_of_transactions_for_regex():
             "date": "2018-08-19T04:27:37.904916",
             "operationAmount": {
                 "amount": "56883.54",
-                "currency": {
-                    "name": "USD",
-                    "code": "USD"
-                }
+                "currency": {"name": "USD", "code": "USD"},
             },
             "description": "Перевод с карты на карту",
             "from": "Visa Classic 6831982476737658",
-            "to": "Visa Platinum 8990922113665229"
+            "to": "Visa Platinum 8990922113665229",
         },
         {
             "id": 596171168,
@@ -339,14 +299,11 @@ def list_of_transactions_for_regex():
             "date": "2018-07-11T02:26:18.671407",
             "operationAmount": {
                 "amount": "79931.03",
-                "currency": {
-                    "name": "руб.",
-                    "code": "RUB"
-                }
+                "currency": {"name": "руб.", "code": "RUB"},
             },
             "description": "Открытие вклада",
-            "to": "Счет 72082042523231456215"
-        }
+            "to": "Счет 72082042523231456215",
+        },
     ]
 
 
@@ -359,14 +316,11 @@ def list_of_transactions_curr_empty():
             "date": "2018-06-30T02:08:58.425572",
             "operationAmount": {
                 "amount": "9824.07",
-                "currency": {
-                    "name": "",
-                    "code": ""
-                }
+                "currency": {"name": "", "code": ""},
             },
             "description": "Перевод организации",
             "from": "Счет 75106830613657916952",
-            "to": "Счет 11776614605963066702"
+            "to": "Счет 11776614605963066702",
         }
     ]
 
@@ -380,14 +334,11 @@ def list_of_transactions_no_curr():
             "date": "2018-06-30T02:08:58.425572",
             "operationAmount": {
                 "amount": "9824.07",
-                "currency": {
-                    "name": "EUR",
-                    "code": "EUR"
-                }
+                "currency": {"name": "EUR", "code": "EUR"},
             },
             "description": "Перевод организации",
             "from": "Счет 75106830613657916952",
-            "to": "Счет 11776614605963066702"
+            "to": "Счет 11776614605963066702",
         }
     ]
 
@@ -405,14 +356,11 @@ def some_transaction_usd():
         "date": "2018-06-30T02:08:58.425572",
         "operationAmount": {
             "amount": "1500.6",
-            "currency": {
-                "name": "USD",
-                "code": "USD"
-            }
+            "currency": {"name": "USD", "code": "USD"},
         },
         "description": "Перевод организации",
         "from": "Счет 75106830613657916952",
-        "to": "Счет 11776614605963066702"
+        "to": "Счет 11776614605963066702",
     }
 
 
@@ -424,12 +372,9 @@ def some_transaction_rub():
         "date": "2018-06-30T02:08:58.425572",
         "operationAmount": {
             "amount": "9824.07",
-            "currency": {
-                "name": "руб.",
-                "code": "RUB"
-            }
+            "currency": {"name": "руб.", "code": "RUB"},
         },
         "description": "Перевод организации",
         "from": "Счет 75106830613657916952",
-        "to": "Счет 11776614605963066702"
+        "to": "Счет 11776614605963066702",
     }
